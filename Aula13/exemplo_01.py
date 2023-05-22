5c9db541c09be1e15c90e2988d3fa11fd5cb1a5d
Christoff de Lima Dornelas Cavalcante <Chriss@academicos.com>
2023-05-22 20:57:30 -0300

    Aula 13

---
+++ Aula13/exemplo_01.py
@@ -0,0 +1,12 @@
+def soma(num1, num2):
+    resultado = num1 + num2
+    return resultado
+
+#print(soma(8, 3))
+#print(soma(int(input('Digite o num1: ')), int(input('Digite o num2: '))))
+resultado_1 = soma(3, 5)
+print(f'Resultado 1 é igual a {resultado_1}')
+resultado_2 = soma(10, 5)
+print(f'Resultado 2 é igual a {resultado_2}')
+resultado_3 = soma(3, 6789)
+print(f'Resultado 3 é igual a {resultado_3}')
