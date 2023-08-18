32c00268975a8776d69a2c01921c246967ee27ff
Christoff Dornelas <126968171+dornelxs@users.noreply.github.com>
2023-08-18 20:48:20 -0300

    Delete Aula_7 directory

--- Aula_7/exercicio_10.py
+++
@@ -1,9 +0,0 @@
-nomes = ["duda", "chris", "duda", "chris", "carvalho", "dilba"]
-
-nomes_n_duplicados = []
-
-for nome in nomes:
-    if nome not in nomes_n_duplicados:
-        nomes_n_duplicados.append(nome)
-
-print(nomes_n_duplicados)
