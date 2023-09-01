0fc205a81432eec52d2ca75b6e45bcb6515f05e8
Christoff Dornelas <126968171+dornelxs@users.noreply.github.com>
2023-09-01 19:57:03 -0300

    Delete Aula_7 directory

--- Aula_7/exercicio_1.py
+++
@@ -1,8 +0,0 @@
-#Fácil: Faça um programa que leia dois números inteiros e exiba o maior deles.#
-n1 = 2
-n2 = 9
-if n1 >= n2:
-   print(f'O número {n2} é maior!')
-else:
-    print(f'O número {n2} é maior que o número {n1}!')
-
--- Aula_7/exercicio_8.py
+++
@@ -1,5 +0,0 @@
-P = input("Digite uma palavra: ")
-if P == P[::-1]:
-    print("A palavra é um palíndromo.")
-else:
-    print("A palavra não é um palíndromo.")
